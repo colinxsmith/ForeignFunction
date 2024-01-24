5d0dc4fbafcafe8a062f5ed1be659fe7ed850f6e
Colin Smith <colinxsmith@gmail.com>
2024-01-24 23:34:29 +0000

    .

--- testfactormodelprocess.java
+++ testfactormodelprocess.java
@@ -1,9 +1,11 @@
 import com.bitaplus.BitaModel.Optimisation.*;
+
 //compile via;    javac  --enable-preview  --source 21 -Xlint:preview  testfactormodelprocess.java
-//run via;         java  --enable-preview --enable-native-access=ALL-UNNAMED --source 21 .\testfactormodelprocess.java
+//run via;        java  -ea --enable-preview --enable-native-access=ALL-UNNAMED --source 21 .\testfactormodelprocess.java
 public class testfactormodelprocess {
     public static void main(String args[]) {
-        double[][] FL = { { 1, 1 }, { 2, -1}, { 3, 1 }, { 4, -1 } };
+        // OptimiserFunctions.libraryname = "/host/colin/safeqp/libsafeqp.so";
+        double[][] FL = { { 1, 1 }, { 2, -1 }, { 3, 1 }, { 4, -1 } };
         int n = FL.length;
         int nf = FL[0].length;
         double[] SV = { 4, 5, 6, 7, 8 };
@@ -15,7 +17,7 @@ public class testfactormodelprocess {
         OptimiserFunctions.dsubvec((nf + 1) * n, Q, Q1, Q1);
         double diff = OptimiserFunctions.ddotvec((nf + 1) * n, Q1, Q1);
         assert diff < OptimiserFunctions.lm_eps;
-        System.out.println("Agreement difference "+diff);
+        System.out.println("Agreement difference " + diff);
         OptimiserFunctions.daddvec((nf + 1) * n, Q, Q1, Q1);
     }
 }
